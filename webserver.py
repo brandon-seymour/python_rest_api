e740e7e64587ca965fef710c12480673c3fa08e0
brandon-seymour <59863256+brandon-seymour@users.noreply.github.com>
2023-05-20 03:28:53 -0700

    custom exceptions instead of multiple return values

--- webserver.py
+++ webserver.py
@@ -1,102 +1,83 @@
 from http.server import *
 import re
+import os
+
+class RequestException(Exception):
+    def __init__(self, status_code, message):
+        super().__init__(message)
+        self.status_code = status_code
+        self.message = message   
 
 class ResponseHandler(BaseHTTPRequestHandler):
-
     def __init__(self, args, directory, kwargs):
         self.path_function_registry = {
-            'test' : self.test
+            'test' : test
         }
         super().__init__(args, directory, kwargs)
 
-    def test(self, value):
-        self.send_response(200)
-        self.send_header('content-type', 'text/html')
-        self.end_headers()
-        self.wfile.write('test complete'.encode())
-
     def _validate_path(self):
-        status_code = 200
-        message = None
-
-        if self.path[0] != '/':
-            status_code = 500
-            message = 'first character of request was not /'
-        
-        if self.path.count('/') > 2:
-            status_code = 400
-            message = 'too many /'
-        
-        pattern = '(?<=\/).*(?=\/)'
-        path = re.findall(pattern, self.path)
-        
+        if self.path.count('/') != 2:
+            raise RequestException(400, 'request path should be /command/')
+        path = re.findall('(?<=\/).*(?=\/)', self.path)
         if len(path) == 0:
-            status_code = 400
-            message = 'path is too short'
+            raise RequestException(400, 'path is too short')
         elif len(path) > 1:
-            status_code = 400
-            message = 'path is too long'
+            raise RequestException(400, 'path is too long')
         else:
             path = path[0]
-            print(path)
-            print('registry = ', self.path_function_registry)
             if not self.path_function_registry.get(path):
-                status_code = 400
-                message = 'command not supported'
-        
-        return (status_code, message, path)
+                raise RequestException(400, 'command not supported')
+        return path
 
     def _validate_params(self, query_param_string):
-        status_code = 200
-        message     = None
-
         if query_param_string[1] != '?':
-            status_code = 400
-            message = "? not in query parameter string"
-        
+            raise RequestException(400, "? not in query parameter string")
         terminator  = '$' if '&' not in query_param_string else '&'
         pattern     = '(?<=\?).*(?=%s)' % terminator
         param_list  = re.findall(pattern, query_param_string)
-
         if not param_list:
-            status_code = 400
-            message = "no query parameters found that match regex pattern %s" % pattern
-        
+            raise RequestException(400, "no query parameters found that match \
+                                    regex pattern %s" % pattern)
         query_params = {}
-
         for kv_string in param_list:
             if '=' not in kv_string:
-                status_code = 400
-                message = "= not in query parameter string"
+                raise RequestException(400, "= not in query parameter string")
             split = kv_string.split('=')
             k = split[0]
             v = split[1]
             if not len(k) or not len(v):
-                status_code = 400
-                message = "key or value is empty"
+                raise RequestException(400, "key or value is empty")
             else:
                 query_params[k] = v
-
-        print(query_params)
-        return (status_code, message, query_params)
-              
+        return query_params
+    
     def do_GET(self):
-        status_code, message, path = self._validate_path()
-        if status_code != 200:
-            self.send_error(status_code, message)
+        try:
+            path = self._validate_path()
+            query_param_string = self.path.split(path)[1]
+            query_params  = self._validate_params(query_param_string)
+            func = self.path_function_registry.get(path)
+            headers, response = func(query_params)
+            self.send_response(200)
+            for k, v in headers.items():
+                self.send_header(k, v)
+            self.end_headers()
+            self.wfile.write(response.encode())
+        except RequestException as e:
+            self.send_error(e.status_code, e.message)
             return
-        
-        query_param_string = self.path.split(path)[1]
-        status_code, message, query_params  = self._validate_params(query_param_string)
+        except Exception as e:
+            self.send_error(500, 'python api internal server error')
 
-        if status_code != 200:
-            self.send_error(status_code, message)
-            return
-        
-        print('registry = ', self.path_function_registry)
-        print('path = ', path)
-        func = self.path_function_registry.get(path)
-        func(query_params)
-
-port = HTTPServer(('', 5555), ResponseHandler)
-port.serve_forever()
+def test(query_params):
+    this_dir = os.path.dirname(os.path.realpath(__file__))
+    test_file = os.path.join(this_dir, 'test.txt')
+    headers = {
+        'content-type': 'text/html'
+    }
+    with open(test_file, 'r') as f:
+        response = "\r\n".join(f.readlines())
+    return (headers, response)
+    
+server = HTTPServer(('', 5555), ResponseHandler)
+server.serve_forever()
